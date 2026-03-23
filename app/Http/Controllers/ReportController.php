d0ed2afa446e7f2310de24c950edb3953e20a03f
Suraj <surajrawal.androapps@gmail.com>
2026-03-23 14:09:05 +0530

    Report

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\PayrollEntry;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Inertia\Inertia;
 
 class ReportController extends Controller
@@ -43,6 +44,8 @@ class ReportController extends Controller
                 "designations.name as designation",
                 "departments.name as department",
                 "branches.name as location",
+                "earnings_breakdown",
+                "deductions_breakdown"
             ])->orderBy('payroll_entries.created_at', 'DESC');
 
         $perPage = $request->has('per_page') ? (int)$request->per_page : 10;
@@ -56,8 +59,31 @@ class ReportController extends Controller
 
     public function itax(Request $request)
     {
+
+        $salaryQuery = PayrollEntry::join('users', 'users.id', '=', 'payroll_entries.employee_id')
+            ->join('employees', 'employees.user_id', '=', 'users.id')
+            ->select([
+                "users.name",
+                "employees.employee_id",
+                "total_deductions",
+                "component_earnings",
+                "basic_salary",
+                "deductions_breakdown",
+                DB::raw('JSON_UNQUOTE(JSON_EXTRACT(deductions_breakdown, "$.\"ITax\"")) as itax')
+            ])
+            ->whereRaw('JSON_EXTRACT(deductions_breakdown, "$.\"ITax\"") IS NOT NULL')
+            ->whereRaw('JSON_UNQUOTE(JSON_EXTRACT(deductions_breakdown, "$.\"ITax\"")) != "null"')
+            ->orderBy('payroll_entries.created_at', 'DESC');
+
+        $perPage = $request->has('per_page') ? (int)$request->per_page : 10;
+        $reports = $salaryQuery->paginate($perPage)->withQueryString();
+
+        // dd($salaryQuery);
+
+        // $reports = $salaryQuery->paginate($perPage)->withQueryString();
+
         return Inertia::render('reports/itax', [
-            'reports' => [],
+            'reports' => $reports,
             'filters' => $request->all(['search', 'sort_field', 'sort_direction', 'per_page']),
         ]);
     }
@@ -72,16 +98,50 @@ class ReportController extends Controller
 
     public function pf(Request $request)
     {
+        $salaryQuery = PayrollEntry::join('users', 'users.id', '=', 'payroll_entries.employee_id')
+            ->join('employees', 'employees.user_id', '=', 'users.id')
+            ->select([
+                "users.name",
+                "employees.employee_id",
+                "total_deductions",
+                "component_earnings",
+                "basic_salary",
+                "deductions_breakdown",
+                DB::raw('JSON_UNQUOTE(JSON_EXTRACT(deductions_breakdown, "$.\"Provident Fund\"")) as pf')
+            ])
+            ->whereRaw('JSON_EXTRACT(deductions_breakdown, "$.\"Provident Fund\"") IS NOT NULL')
+            ->whereRaw('JSON_UNQUOTE(JSON_EXTRACT(deductions_breakdown, "$.\"Provident Fund\"")) != "null"')
+            ->orderBy('payroll_entries.created_at', 'DESC');
+
+        $perPage = $request->has('per_page') ? (int)$request->per_page : 10;
+        $reports = $salaryQuery->paginate($perPage)->withQueryString();
         return Inertia::render('reports/pf', [
-            'reports' => [],
+            'reports' => $reports,
             'filters' => $request->all(['search', 'sort_field', 'sort_direction', 'per_page']),
         ]);
     }
 
     public function esi(Request $request)
     {
+        $salaryQuery = PayrollEntry::join('users', 'users.id', '=', 'payroll_entries.employee_id')
+            ->join('employees', 'employees.user_id', '=', 'users.id')
+            ->select([
+                "users.name",
+                "employees.employee_id",
+                "total_deductions",
+                "component_earnings",
+                "basic_salary",
+                "deductions_breakdown",
+                DB::raw('JSON_UNQUOTE(JSON_EXTRACT(deductions_breakdown, "$.\"ESI\"")) as esi')
+            ])
+            ->whereRaw('JSON_EXTRACT(deductions_breakdown, "$.\"ESI\"") IS NOT NULL')
+            ->whereRaw('JSON_UNQUOTE(JSON_EXTRACT(deductions_breakdown, "$.\"ESI\"")) != "null"')
+            ->orderBy('payroll_entries.created_at', 'DESC');
+
+        $perPage = $request->has('per_page') ? (int)$request->per_page : 10;
+        $reports = $salaryQuery->paginate($perPage)->withQueryString();
         return Inertia::render('reports/esi', [
-            'reports' => [],
+            'reports' => $reports,
             'filters' => $request->all(['search', 'sort_field', 'sort_direction', 'per_page']),
         ]);
     }
